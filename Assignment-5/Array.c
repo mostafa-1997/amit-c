544b234f0310cfe796aaf50db941268437732e4f
Mostafa <mostax266@gmail.com>
2021-08-09 15:27:25 +0200

    assignment-5

---
+++ Assignment-5/Array.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+//Function Prototypes
+void scanArrayValues(int arr[],int);
+void printArray(int arr[],int);
+void arrayMax(int arr[],int);
+void arrayMin(int arr[],int);
+void sortArrayAscending(int arr[],int);
+
+int main(){
+    int arr_1[5] = {}; //All values will be initialized wih 0.
+
+    //get the size of the array
+    int size = sizeof(arr_1)/sizeof(arr_1[0]);
+    //scan the array values
+    scanArrayValues(arr_1,size);
+    //print the 5 elements of the array
+    printArray(arr_1,size);
+    //get the maximum number in array and its position
+    arrayMax(arr_1,size);
+    //get the minimum number in array and its position
+    arrayMin(arr_1,size);
+    //Sort elements of array in ascending order
+    sortArrayAscending(arr_1,size);
+    //print the array
+    printArray(arr_1,size);
+    return 0;
+}
+//Function Definitions
+void scanArrayValues(int arr[],int size){
+    int x;
+    printf("Enter 5 numbers : \n");
+    for(int i=0;i<size;i++){
+        scanf("%d",&x);
+        arr[i] = x;
+    }
+}
+
+void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d \t",arr[i]);
+    }
+    printf("\n");
+}
+
+void arrayMax(int arr[],int size){
+    int max = arr[0];
+    int pos = 0;
+    for(int i=1;i<size;i++){
+        if(arr[i]>max){
+            max = arr[i];
+            pos = i;
+        }
+    }
+    printf("The maximum Number is %d in position %d \n",max,pos);
+}
+
+void arrayMin(int arr[],int size){
+    int min = arr[0];
+    int pos = 0;
+    for(int i=1;i<size;i++){
+        if(arr[i]<min){
+            min = arr[i];
+            pos = i;
+        }
+    }
+    printf("The minimum Number is %d in position %d \n",min,pos);
+}
+
+void sortArrayAscending(int arr[],int size){
+    int temp;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("Array is sorted in Ascending order.\n");
+} 
